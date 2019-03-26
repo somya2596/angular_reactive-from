cf2be47338c23ca0255813343e64fbf340dee41b
somya golchha <42554214+somya2596@users.noreply.github.com>
2019-03-26 16:20:28 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,13 @@ import { FormBuilder,Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  get userName(){
+    return this.registrationForm.get('userName')
+  }
   constructor(private fb:FormBuilder){}
    registrationForm = this.fb.group({
-      userName:['',Validators.required],
+      userName:["",[Validators.required,Validators.minLength(3)]],
     password:[''],
     confirmPassword:[''],
     address: this.fb.group({
